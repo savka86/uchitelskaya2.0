5c7d1edd16a50526c12b654b5a321849be1f6eb9
savka86 <64674641+savka86@users.noreply.github.com>
2026-08-23 22:48:46 +0900

    Bump PWA shell cache after path fix

--- fuel-radar/service-worker.js
+++ fuel-radar/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='radar-azs-shell-v5';
+const CACHE='radar-azs-shell-v6';
 const SHELL=['./','./pwa.html','./pwa-photo.js','./index.html','./privacy.html','./rights.html','./manifest.webmanifest','./pwa-icon.svg','./data/camera-status.json','./version.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
