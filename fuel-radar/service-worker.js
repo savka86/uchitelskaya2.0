fdc292a2fa1c98f5c7a28b211e8ae49644373b5d
savka86 <64674641+savka86@users.noreply.github.com>
2026-08-25 17:57:46 +0900

    Bump PWA cache for theme update

--- fuel-radar/service-worker.js
+++ fuel-radar/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='radar-azs-shell-v10';
+const CACHE='radar-azs-shell-v11';
 const SHELL=['/','/pwa.html','/pwa-bootstrap.js','/pwa-photo-v8.js','/index.html','/privacy.html','/rights.html','/manifest.webmanifest','/pwa-icon.svg','/version.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
