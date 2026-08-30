eb2088cc9adde6e804685104e288d89098b8684b
savka86 <64674641+savka86@users.noreply.github.com>
2026-08-30 22:59:13 +0900

    SAVTOBUSRASP: use road-following Yandex route

--- savtobusrasp/app/page.tsx
+++ savtobusrasp/app/page.tsx
@@ -1,9 +1,9 @@
-import { TransitAppYandex } from "@/components/TransitAppYandex";
+import { TransitAppRoad } from "@/components/TransitAppRoad";
 import { loadSchedule } from "@/lib/schedule";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const schedule = await loadSchedule();
-  return <TransitAppYandex initialData={schedule} />;
+  return <TransitAppRoad initialData={schedule} />;
 }
