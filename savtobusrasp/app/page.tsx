761dfb11c831642bb4b0a81886003d7adc3a79b5
savka86 <64674641+savka86@users.noreply.github.com>
2026-08-30 22:30:40 +0900

    SAVTOBUSRASP: switch to Yandex-only route

--- savtobusrasp/app/page.tsx
+++ savtobusrasp/app/page.tsx
@@ -1,9 +1,9 @@
-import { TransitAppV2 } from "@/components/TransitAppV2";
+import { TransitAppYandex } from "@/components/TransitAppYandex";
 import { loadSchedule } from "@/lib/schedule";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const schedule = await loadSchedule();
-  return <TransitAppV2 initialData={schedule} />;
+  return <TransitAppYandex initialData={schedule} />;
 }
