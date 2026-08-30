83431cb4b8dd2f31d6e2eec1ec1d9a6cfe9c703b
savka86 <64674641+savka86@users.noreply.github.com>
2026-08-30 23:37:12 +0900

    SAVTOBUSRASP: switch to manual road route

--- savtobusrasp/app/page.tsx
+++ savtobusrasp/app/page.tsx
@@ -1,9 +1,9 @@
-import { TransitAppRoadSegments } from "@/components/TransitAppRoadSegments";
+import { TransitAppManual } from "@/components/TransitAppManual";
 import { loadSchedule } from "@/lib/schedule";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const schedule = await loadSchedule();
-  return <TransitAppRoadSegments initialData={schedule} />;
+  return <TransitAppManual initialData={schedule} />;
 }
