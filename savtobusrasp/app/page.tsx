db3c9d79c21b66897dbda63c8e8510e4ce263101
savka86 <64674641+savka86@users.noreply.github.com>
2026-08-31 00:21:49 +0900

    SAVTOBUSRASP: switch to QR route component

--- savtobusrasp/app/page.tsx
+++ savtobusrasp/app/page.tsx
@@ -1,9 +1,9 @@
-import { TransitAppManual } from "@/components/TransitAppManual";
+import { TransitAppQR } from "@/components/TransitAppQR";
 import { loadSchedule } from "@/lib/schedule";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const schedule = await loadSchedule();
-  return <TransitAppManual initialData={schedule} />;
+  return <TransitAppQR initialData={schedule} />;
 }
