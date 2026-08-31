6189cc51b6c8f9311001a9bb996ce2cf5f33543a
savka86 <64674641+savka86@users.noreply.github.com>
2026-08-31 12:13:11 +0900

    SAVTOBUSRASP: show routes 1 and 2 on same site

--- savtobusrasp/app/page.tsx
+++ savtobusrasp/app/page.tsx
@@ -1,9 +1,13 @@
-import { TransitAppAttached } from "@/components/TransitAppAttached";
+import { TransitRouteSwitcher } from "@/components/TransitRouteSwitcher";
 import { loadSchedule } from "@/lib/schedule";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const schedule = await loadSchedule();
-  return <TransitAppAttached initialData={schedule} />;
+  const [route1, route2] = await Promise.all([
+    loadSchedule("namtsy-1"),
+    loadSchedule("namtsy-2"),
+  ]);
+
+  return <TransitRouteSwitcher route1={route1} route2={route2} />;
 }
